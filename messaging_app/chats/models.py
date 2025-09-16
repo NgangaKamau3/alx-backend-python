bb23b258e719d0f1045c555bced8ad052255626d
NgangaKamau3 <ngangajohn23405@gmail.com>
2025-09-16 19:27:54 +0300

    Add chat models for the messaging app

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -34,6 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 	email = models.EmailField(max_length=255, unique=True)
 	first_name = models.CharField(max_length=150, blank=True)
 	last_name = models.CharField(max_length=150, blank=True)
+	phone_number = models.CharField(max_length=20, blank=True, null=True)
 	date_joined = models.DateTimeField(default=timezone.now)
 	is_active = models.BooleanField(default=True)
 	is_staff = models.BooleanField(default=False)
